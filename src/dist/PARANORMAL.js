ebdd3a036ef63cc9252289f0b0fc5a5699970051
Ben Boyle <benjamins.boyle@gmail.com>
2013-02-17 03:03:34 +1000

    add sample files
    sample project files and QUnit test (failing)

---
+++ src/dist/PARANORMAL.js
@@ -0,0 +1,18 @@
+/*! PARANORMAL - v0.1.0 - 2013-02-17
+* https://github.com/bboyle/paranormal
+* Copyright (c) 2013 Ben Boyle; Licensed MIT */
+
+/*! THIS BANNER WILL BE REMOVED BY GRUNT CONCAT
+
+	Try it and see
+
+*/
+
+(function() {
+	'use strict';
+
+	var foo = 'foo';
+
+	// THIS FILE IS A TEST ONLY
+	
+}());
---
+++ src/lib/PARANORMAL.js
@@ -0,0 +1,14 @@
+/*! THIS BANNER WILL BE REMOVED BY GRUNT MINIFICATION
+
+	Try it and see
+
+*/
+
+(function() {
+	'use strict';
+
+	var foo = 'foo';
+
+	// THIS FILE IS A TEST ONLY
+	
+}());
