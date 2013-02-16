0269ac9232aea2eced1e8aee3a70ee4780f195f9
Ben Boyle <benjamins.boyle@gmail.com>
2013-02-17 03:03:10 +1000

    added phantomjs
    updated build and profile for phantomjs
    fix 'grunt' command line alias

--- profile.cmd
+++ profile.cmd
@@ -6,5 +6,11 @@ set PATH=%~d0%~p0lib\apache-ant-1.8.4\bin;%PATH%
 echo adding nodejs to path
 set PATH=%~d0%~p0lib\node;%PATH%
 
+echo fixing grunt command
+doskey grunt=grunt.cmd
+
+echo adding phantomjs to path
+set PATH=%~d0%~p0lib\phantomjs\phantomjs-1.8.1-windows;%PATH%
+
 echo ready
 cls
