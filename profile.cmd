688a4398604d64c308d6f94a04a2f3ec90f0231f
Ben Boyle <benjamins.boyle@gmail.com>
2013-02-17 22:19:26 +1000

    add ruby variant of cucumber
    added gemfile for bundler
    added WAC for ANSI color support on windows command window
    generate cucumber.cmd alias that pipes output through wac

--- profile.cmd
+++ profile.cmd
@@ -15,5 +15,8 @@ set PATH=%~d0%~p0lib\phantomjs\phantomjs-1.8.1-windows;%PATH%
 echo adding jruby to path
 set PATH=%~d0%~p0lib\jruby\jruby-1.7.2\bin;%PATH%
 
+echo adding WAC to path
+set PATH=%~d0%~p0lib\wac;%PATH%
+
 echo ready
 cls
