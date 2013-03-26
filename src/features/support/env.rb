3bbb275dc9eaf33eec814bbcee86073f97e2558d
Ben Boyle <benjamins.boyle@gmail.com>
2013-03-26 21:57:09 +1000

    fix build
    - remove comments
    - specify lib dir for node commands
    - replace 'restart' target with 'all'

--- src/features/support/env.rb
+++ src/features/support/env.rb
@@ -1,9 +1,5 @@
 require 'watir-webdriver'
 
-# PORTABLE_APPS = 'C:\PortableApps'
-# Selenium::WebDriver::Firefox.path = PORTABLE_APPS + '\FirefoxPortable\FirefoxPortable.exe'
-# Selenium::WebDriver::Chrome.path = PORTABLE_APPS + '\GoogleChromePortable\GoogleChromePortable.exe'
-
 browser = Watir::Browser.new
  
 Before do
