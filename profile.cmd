f34bd0bf916f3aee97b7b693dc033df08dc9400e
Ben Boyle <benjamins.boyle@gmail.com>
2014-01-09 20:35:25 +1000

    updated libs
    - removed python
    - node 1.10.24, npm 1.3.15
    - grunt jshint 0.8.0
    - replace beautify with jsbeautifier
    - grunt copy 0.5.0
    - remove syntax gem
    - update gems

--- profile.cmd
+++ profile.cmd
@@ -4,20 +4,17 @@ echo set DEV environment
 set TEST_ENV=DEV
 
 echo adding ant to path
-set PATH=%~d0%~p0lib\apache-ant-1.9.0\bin;%PATH%
+set PATH=%~d0%~p0lib\apache-ant-1.9.3\bin;%PATH%
 
 echo adding nodejs to path
 set PATH=%~d0%~p0lib\node;%PATH%
 
 echo adding jruby to path
-set PATH=%~d0%~p0lib\jruby\jruby-1.7.3\bin;%PATH%
+set PATH=%~d0%~p0lib\jruby\jruby-1.7.9\bin;%PATH%
 
 echo adding webdriver to path
 set PATH=%~d0%~p0lib\chromedriver;%PATH%
 set PATH=%~d0%~p0lib\IEDriverServer;%PATH%
-
-echo adding python to path
-set PATH=%~d0%~p0lib\python\Portable Python 3.2.5.1\App\;%~d0%~p0lib\python\Portable Python 3.2.5.1\App\Scripts\;%PATH%
 
 echo linux command aliases
 doskey ls=dir
